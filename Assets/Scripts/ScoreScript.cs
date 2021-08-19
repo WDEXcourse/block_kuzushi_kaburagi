87626788eaab720f84ae98559ba90b39b3383405
WDEXcourse <wdkawasaki.online@gmail.com>
2021-08-19 19:35:52 +0900

    Initial commit

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public int Score;
+    //private int Score;
+    public Text ScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Score = BlockDestroy.ScoreCount;
+        ScoreText.text = "Score:" + Score.ToString();
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    Score += 1000;
+        //}
+    }
+}
